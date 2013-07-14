3e53afba3c38bce05cbfabfdd57e7903e5e42ad9
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2013-07-14 20:07:51 +0100

    Added a sample template for new samples

---
+++ Sample-Template/Game.cs
@@ -0,0 +1,94 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// <SampleGameName>.cs
+//
+// Description and license
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input.Touch;
+
+
+namespace <SampleNamespace>
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class <SampleGameName> : Microsoft.Xna.Framework.Game
+    {
+        // Resources for drawing.
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        
+        public <SampleGameName>()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+#if WINDOWS_PHONE
+            TargetElapsedTime = TimeSpan.FromTicks(333333);
+#endif
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+ 
+            //Known issue that you get exceptions if you use Media PLayer while connected to your PC
+            //See http://social.msdn.microsoft.com/Forums/en/windowsphone7series/thread/c8a243d2-d360-46b1-96bd-62b1ef268c66
+            //Which means its impossible to test this from VS.
+            //So we have to catch the exception and throw it away
+            try
+            {
+                MediaPlayer.IsRepeating = true;
+                //MediaPlayer.Play(Content.Load<Song>("Sounds/Music"));
+            }
+            catch { }
+
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+				
+            base.Update(gameTime);
+        }
+            --levelIndex;
+            LoadNextLevel();
+        }
+
+        /// <summary>
+        /// Draws the game from background to foreground.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            spriteBatch.Begin();
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+    }
+}
---
+++ Sample-Template/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Platformer2D
+{
+#if !WINDOWS_PHONE
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if WINDOWS || LINUX || PSM
+            using (var game = new PlatformerGame())
+                game.Run();
+
+#else
+            var factory = new MonoGame.Framework.GameFrameworkViewSource<PlatformerGame>();
+            Windows.ApplicationModel.Core.CoreApplication.Run(factory);
+#endif
+        }
+    }
+#endif
+}
