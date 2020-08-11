76a176cc98d6d1d0e4359903e750bd25e46f9a39
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-08-11 17:26:55 +0100

    Updated Android NeonShooter platform (needs testing)

--- NeonShooter/Platforms/Android/Properties/AssemblyInfo.cs
+++ NeonShooter/Platforms/Android/Properties/AssemblyInfo.cs
@@ -1,26 +1,19 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
-#if ANDROID
 using Android.App;
-#endif
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Platformer2D")]
-[assembly: AssemblyProduct("Platformer2D")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("NeonShooter")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("NeonShooter")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
@@ -35,9 +28,3 @@ using Android.App;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
-#if ANDROID
-// Add some common permissions, these can be removed if not needed
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-#endif
