446e8022a18401aac3b9299e01b123c6f7feea01
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-08-18 19:52:48 +0100

    Replaced Platformer2D sample with new template

---
+++ Platformer2D/Platformer2D.DesktopGL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Platformer2D.DesktopGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new PlatformerGame())
+                game.Run();
+        }
+    }
+}
---
+++ Platformer2D/Platformer2D.WindowsDX/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Platformer2D.WindowsDX
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new PlatformerGame())
+                game.Run();
+        }
+    }
+}
