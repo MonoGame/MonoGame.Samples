81590f5b4cc45091a3868659cfa174a23a43e8d7
Simon Jackson <darkside@zenithmoon.com>
2022-10-11 16:01:56 +0100

    Added AutoPong UWP sample

---
+++ AutoPong/AutoPong.WindowsUWP/GamePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace AutoPong.WindowsUWP
+{
+    public sealed partial class GamePage : Page
+    {
+        readonly AutoPongGame _game;
+
+        public GamePage()
+        {
+            this.InitializeComponent();
+
+            // Create the game.
+            var launchArguments = string.Empty;
+            _game = MonoGame.Framework.XamlGame<AutoPongGame>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
+        }
+    }
+}
