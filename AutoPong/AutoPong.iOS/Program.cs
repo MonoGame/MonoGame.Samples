d2c11314949b5cd59f8b7ae059ab74011e4fde79
Simon Jackson <darkside@zenithmoon.com>
2022-10-11 15:56:57 +0100

    Added AutoPong iOS sample

---
+++ AutoPong/AutoPong.iOS/Program.cs
@@ -0,0 +1,30 @@
+﻿using Foundation;
+using UIKit;
+
+namespace AutoPong.iOS
+{
+    [Register("AppDelegate")]
+    internal class Program : UIApplicationDelegate
+    {
+        private static AutoPongGame game;
+
+        internal static void RunGame()
+        {
+            game = new AutoPongGame();
+            game.Run();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, typeof(Program));
+        }
+
+        public override void FinishedLaunching(UIApplication app)
+        {
+            RunGame();
+        }
+    }
+}
