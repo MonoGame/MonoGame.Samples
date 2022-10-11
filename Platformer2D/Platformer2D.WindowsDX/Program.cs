42e4e6b5dfb292290ac73744c377798146909cb6
Simon Jackson <darkside@zenithmoon.com>
2022-10-11 13:33:22 +0100

    Initial check-in of Platformer 2D update to 3.8.1

--- Platformer2D/Platformer2D.WindowsDX/Program.cs
+++ Platformer2D/Platformer2D.WindowsDX/Program.cs
@@ -1,14 +1,5 @@
-﻿using System;
+﻿
+using Platformer2D;
 
-namespace Platformer2D.WindowsDX
-{
-    public static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            using (var game = new PlatformerGame())
-                game.Run();
-        }
-    }
-}
+using var game = new PlatformerGame();
+game.Run();
