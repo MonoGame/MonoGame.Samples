ba271b9eac3177da44af6c6627fdd77490680cf5
Hyunwook Ha <hyunwookha@gmail.com>
2025-06-20 09:12:04 +0900

    Add index++ to 2D Tutorial's AudioController.Update()

--- Tutorials/learn-monogame-2d/src/22-Snake-Game-Mechanics/MonoGameLibrary/Audio/AudioController.cs
+++ Tutorials/learn-monogame-2d/src/22-Snake-Game-Mechanics/MonoGameLibrary/Audio/AudioController.cs
@@ -109,9 +109,9 @@ public class AudioController : IDisposable
             if (instance.State == SoundState.Stopped && !instance.IsDisposed)
             {
                 instance.Dispose();
+                _activeSoundEffectInstances.RemoveAt(index);
             }
-
-            _activeSoundEffectInstances.RemoveAt(index);
+            index++;
         }
     }
 
