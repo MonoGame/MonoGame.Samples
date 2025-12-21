a4a10c11ce220d67152781c886b9c1fa4fa5841b
Victor Chelaru <VicChelaru@gmail.com>
2025-12-21 06:37:28 -0700

    Oops, forgot to check some of the later chapters, found inconsistencies.

--- Tutorials/learn-monogame-2d/src/27-Conclusion/DungeonSlime/Game1.cs
+++ Tutorials/learn-monogame-2d/src/27-Conclusion/DungeonSlime/Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Media;
 using MonoGameLibrary;
 using MonoGameGum;
+using Gum.Forms;
 using Gum.Forms.Controls;
 
 namespace DungeonSlime;
@@ -33,7 +34,7 @@ public class Game1 : Core
     private void InitializeGum()
     {
         // Initialize the Gum service
-        GumService.Default.Initialize(this);
+        GumService.Default.Initialize(this, DefaultVisualsVersion.V3);
 
         // Tell the Gum service which content manager to use.  We will tell it to
         // use the global content manager from our Core.
