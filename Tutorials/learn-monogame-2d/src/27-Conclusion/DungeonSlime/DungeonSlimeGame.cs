3f86c49cfd0832a4fabd1334bb0a342f83f36544
Dominique Louis <savagesoftware@gmail.com>
2026-01-06 12:55:38 +0000

    Add Multi Platform CI (#105)
    * Ensure we build our samples on as many platforms as those projects support, so we can use these on itch.io too.
    
    * Use MonoPack our Desktop target for Linux, macOS and Windows. 
    
    * Build Native WindowsDX
    
    * Build Android apx artifacts for sideloading.

--- Tutorials/learn-monogame-2d/src/27-Conclusion/DungeonSlime/DungeonSlimeGame.cs
+++ Tutorials/learn-monogame-2d/src/27-Conclusion/DungeonSlime/DungeonSlimeGame.cs
@@ -7,12 +7,12 @@ using Gum.Forms.Controls;
 
 namespace DungeonSlime;
 
-public class Game1 : Core
+public class DungeonSlimeGame : Core
 {
     // The background theme song
     private Song _themeSong;
 
-    public Game1() : base("Dungeon Slime", 1280, 720, false)
+    public DungeonSlimeGame() : base("Dungeon Slime", 1280, 720, false)
     {
 
     }
--- Tutorials/learn-monogame-2d/src/27-Conclusion/DungeonSlime/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿using var game = new DungeonSlime.Game1();
-game.Run();
